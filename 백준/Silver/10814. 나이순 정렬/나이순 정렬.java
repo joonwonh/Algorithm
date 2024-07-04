06ea1ee6258110e345fdab2f233a4adb1e4f0f78
JWHan <96328871+joonwonh@users.noreply.github.com>
2024-07-04 15:42:53 +0900

    [Silver V] Title: 나이순 정렬, Time: 1584 ms, Memory: 59984 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        String [][] arr = new String[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = st.nextToken();
+            }
+        }
+
+        Arrays.sort(arr, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] arr1, String[] arr2) {
+                return Integer.parseInt(arr1[0]) - Integer.parseInt(arr2[0]);
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
