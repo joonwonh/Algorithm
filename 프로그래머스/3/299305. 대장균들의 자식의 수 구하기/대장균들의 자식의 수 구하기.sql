89139b08d720ed36c03d19cf624bd021839a31f9
JWHan <96328871+joonwonh@users.noreply.github.com>
2024-07-30 20:42:58 +0900

    [level 3] Title: 대장균들의 자식의 수 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
@@ -0,0 +1,18 @@
+select
+    id,
+    ifnull(
+        (
+            select
+                count(*)
+            from
+                ecoli_data
+            group by
+                parent_id
+            having
+                parent_id = id
+        ), 0
+    ) as child_count
+from
+    ecoli_data
+order by
+    id;
